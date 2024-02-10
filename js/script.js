e1a941d8d2c6d210d384c343ab9cebe8cd8ee3e0
yousssef1212 <123217294+yousssef1212@users.noreply.github.com>
2024-02-10 17:12:56 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ const preloader = document.querySelector(".preloader");
 window.addEventListener('load', () => {
     preloader.remove();
 })
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2211563224767744"
+     crossorigin="anonymous"></script>
